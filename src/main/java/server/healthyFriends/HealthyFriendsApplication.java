85f3a6a096d687c13ef3bd93967cd9bc77b18659
DDeo99 <yoondukwoo@gmail.com>
2023-11-07 14:10:00 +0900

    first commit

---
+++ src/main/java/server/healthyFriends/HealthyFriendsApplication.java
@@ -0,0 +1,13 @@
+package server.healthyFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/healthyFriends/HealthyFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package server.healthyFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
