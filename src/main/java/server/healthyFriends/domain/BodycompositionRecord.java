d3a1f292985a66ad50a6dd18392a24e2741cdb5a
DDeo99 <yoondukwoo@gmail.com>
2023-11-19 01:47:21 +0900

    체성분 기록 엔티티

---
+++ src/main/java/server/healthyFriends/domain/BodycompositionRecord.java
@@ -0,0 +1,39 @@
+package server.healthyFriends.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name="BodycompositionRecord")
+@Table(name="bodycomposition_record")
+public class BodycompositionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(precision = 5, scale = 2)
+    private BigDecimal weight;
+
+    @Column(precision = 3, scale = 1)
+    private BigDecimal skeletal_muscle_mass;
+
+    @Column(precision = 3, scale = 1)
+    private BigDecimal body_fat_mass;
+
+    @Column(precision = 3, scale = 1)
+    private BigDecimal bmi;
+
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+}
