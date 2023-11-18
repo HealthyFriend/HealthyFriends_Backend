f6d4b1d4006d6964fe110c9a2dd27c9ea45d32e2
DDeo99 <yoondukwoo@gmail.com>
2023-11-19 01:47:29 +0900

    목표 엔티티

---
+++ src/main/java/server/healthyFriends/domain/Objective.java
@@ -0,0 +1,37 @@
+package server.healthyFriends.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name="Objective")
+@Table(name="objective")
+public class Objective {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    private LocalDate start_day;
+
+    private LocalDate end_day;
+
+    @Column(length = 20)
+    private String head;
+
+    @Lob
+    private String body;
+
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+}
