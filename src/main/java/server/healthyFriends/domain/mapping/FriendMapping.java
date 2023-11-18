1a6c45164bec37a11eefd3b4b42a6a646d262cd8
DDeo99 <yoondukwoo@gmail.com>
2023-11-19 01:36:50 +0900

    친구 매핑 테이블

---
+++ src/main/java/server/healthyFriends/domain/mapping/FriendMapping.java
@@ -0,0 +1,30 @@
+package server.healthyFriends.domain.mapping;
+
+import lombok.*;
+import server.healthyFriends.domain.User;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name="FriendMapping")
+@Table(name="friend_mapping")
+public class FriendMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long friendId;
+
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id",referencedColumnName = "id")
+    private User user;
+
+}
