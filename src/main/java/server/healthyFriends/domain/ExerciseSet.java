1c121a9c7edfbf865d66301a08ec3e8fa23f7dd4
DDeo99 <yoondukwoo@gmail.com>
2023-11-20 01:18:50 +0900

    운동 세트당 기록 엔티티

---
+++ src/main/java/server/healthyFriends/domain/ExerciseSet.java
@@ -0,0 +1,46 @@
+package server.healthyFriends.domain;
+
+import lombok.*;
+import server.healthyFriends.domain.common.BaseEntity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@Entity(name="ExerciseSet")
+@Table(name="exercise_set")
+public class ExerciseSet extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    private LocalDate date;
+
+    @Column(length = 3)
+    private String exercise_category;
+
+    @Column(length = 15)
+    private String exercise_name;
+
+    private Integer set_number;
+
+    private Integer repetitions;
+
+    @Column(precision = 4,scale = 1)
+    private BigDecimal weight;
+
+    private Boolean status;
+
+    private LocalTime time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="exercise_record_id",referencedColumnName = "id")
+    private ExerciseRecord exerciseRecord;
+
+}
