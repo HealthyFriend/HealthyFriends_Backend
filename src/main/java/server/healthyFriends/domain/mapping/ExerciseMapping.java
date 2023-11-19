6c166909e29dd0eb6e189a5dbaa088de9193ea9c
DDeo99 <yoondukwoo@gmail.com>
2023-11-20 01:18:30 +0900

    운동 매핑 엔티티

---
+++ src/main/java/server/healthyFriends/domain/mapping/ExerciseMapping.java
@@ -0,0 +1,30 @@
+package server.healthyFriends.domain.mapping;
+
+import lombok.*;
+import server.healthyFriends.domain.Exercise;
+import server.healthyFriends.domain.User;
+import server.healthyFriends.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name="ExerciseMapping")
+@Table(name="exercise_mapping")
+public class ExerciseMapping extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id",referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="exercise_id",referencedColumnName = "id")
+    private Exercise exercise;
+}
