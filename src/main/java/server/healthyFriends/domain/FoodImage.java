5bbe9ce08c38eaf44ebe1cbb808b6bd11f3a0c92
DDeo99 <yoondukwoo@gmail.com>
2023-11-20 01:18:58 +0900

    식단 이미지 엔티티

---
+++ src/main/java/server/healthyFriends/domain/FoodImage.java
@@ -0,0 +1,26 @@
+package server.healthyFriends.domain;
+
+import lombok.*;
+import server.healthyFriends.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name="FoodImage")
+@Table(name="food_image")
+public class FoodImage extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @Lob
+    private String image_url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="food_record_id",referencedColumnName = "id")
+    private FoodRecord foodRecord;
+}
