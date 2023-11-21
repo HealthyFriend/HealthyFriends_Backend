e52898f55aab3a086ca52676a679346630b338e0
DDeo99 <yoondukwoo@gmail.com>
2023-11-22 02:02:54 +0900

    Optional findById 추가

--- src/main/java/server/healthyFriends/repository/ObjectiveRepository.java
+++ src/main/java/server/healthyFriends/repository/ObjectiveRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import server.healthyFriends.domain.Objective;
 
+import java.util.Optional;
+
 @Repository
 public interface ObjectiveRepository extends JpaRepository<Objective, Long> {
 
+    Optional<Objective> findById(Long objectiveId);
 }
