233ed8c08e515c77089727c98e3a607c4ae2796d
DDeo99 <yoondukwoo@gmail.com>
2023-11-21 17:24:35 +0900

    ResponseDTO 객체 생성하는 유틸리티 클래스

---
+++ src/main/java/server/healthyFriends/web/response/ResponseUtil.java
@@ -0,0 +1,43 @@
+package server.healthyFriends.web.response;
+
+import server.healthyFriends.domain.dto.ResponseDTO;
+import server.healthyFriends.domain.enums.ResponseStatus;
+
+import server.healthyFriends.domain.dto.ResponseDTO;
+public class ResponseUtil {
+    public static <T> ResponseDTO<T> success(String message, T data) {
+        return new ResponseDTO<>(200, ResponseStatus.OK, message, data);
+    }
+
+    public static <T> ResponseDTO<T> created(String message, T data) {
+        return new ResponseDTO<>(201, ResponseStatus.CREATED, message, data);
+    }
+
+    public static <T> ResponseDTO<T> noContent(String message, T data) {
+        return new ResponseDTO<>(204, ResponseStatus.NO_CONTENT, message, data);
+    }
+
+    public static <T> ResponseDTO<T> badRequest(String message, T data) {
+        return new ResponseDTO<>(400, ResponseStatus.BAD_REQUEST, message, data);
+    }
+
+    public static <T> ResponseDTO<T> unauthorized(String message, T data) {
+        return new ResponseDTO<>(401, ResponseStatus.UNAUTHORIZED, message, data);
+    }
+
+    public static <T> ResponseDTO<T> forbidden(String message, T data) {
+        return new ResponseDTO<>(403, ResponseStatus.FORBIDDEN, message, data);
+    }
+
+    public static <T> ResponseDTO<T> notFound(String message, T data) {
+        return new ResponseDTO<>(404, ResponseStatus.NOT_FOUND, message, data);
+    }
+
+    public static <T> ResponseDTO<T> internalServerError(String message, T data) {
+        return new ResponseDTO<>(500, ResponseStatus.INTERNAL_SERVER_ERROR, message, data);
+    }
+
+    public static <T> ResponseDTO<T> serviceUnavailable(String message, T data) {
+        return new ResponseDTO<>(503, ResponseStatus.SERVICE_UNAVAILABLE, message, data);
+    }
+}
