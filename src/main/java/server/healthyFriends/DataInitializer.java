779f39720ce5ae1191dd282c7447d56a58669c65
DDeo99 <yoondukwoo@gmail.com>
2023-11-22 01:28:53 +0900

    목표 테스트 데이터 추가

--- src/main/java/server/healthyFriends/DataInitializer.java
+++ src/main/java/server/healthyFriends/DataInitializer.java
@@ -1,27 +1,30 @@
 package server.healthyFriends;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import server.healthyFriends.domain.Objective;
 import server.healthyFriends.domain.User;
 import server.healthyFriends.domain.enums.Gender;
 import server.healthyFriends.domain.enums.Role;
+import server.healthyFriends.repository.ObjectiveRepository;
 import server.healthyFriends.repository.UserRepository;
 
 import javax.xml.crypto.Data;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
 
 @Component
+@RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final ObjectiveRepository objectiveRepository;
     private final PasswordEncoder encoder;
 
-    public DataInitializer(UserRepository userRepository, PasswordEncoder encoder) {
-        this.userRepository = userRepository;
-        this.encoder = encoder;
-    }
 
     @Override
     public void run(String... args) {
@@ -36,6 +39,15 @@ public class DataInitializer implements CommandLineRunner {
                 .role(Role.USER)
                 .build();
 
+        Objective objective = Objective.builder()
+                .start_day(LocalDate.now())
+                .end_day(LocalDate.of(2025,01,21))
+                .head("목표 제목")
+                .body("목표 내용")
+                .status(false)
+                .build();
+
         userRepository.save(user);
+        objectiveRepository.save(objective);
     }
 }
