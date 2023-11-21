57654ecc336859ba222801b314d5c52509996d86
DDeo99 <yoondukwoo@gmail.com>
2023-11-22 01:28:27 +0900

    ObjectiveService DI

--- src/main/java/server/healthyFriends/config/AppConfig.java
+++ src/main/java/server/healthyFriends/config/AppConfig.java
@@ -5,13 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import server.healthyFriends.repository.ObjectiveRepository;
 import server.healthyFriends.repository.UserRepository;
+import server.healthyFriends.service.ObjectiveSerivce;
+import server.healthyFriends.service.ObjectiveServiceImpl;
 import server.healthyFriends.service.UserService;
 import server.healthyFriends.service.UserServiceImpl;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class AppConfig {
     //스프링 설정 파일
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCryptPasswordEncoder를 사용하여 비밀번호를 안전하게 저장
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public UserService userService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -19,9 +30,8 @@ public class AppConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        // BCryptPasswordEncoder를 사용하여 비밀번호를 안전하게 저장
-        return new BCryptPasswordEncoder();
+    public ObjectiveSerivce objectiveSerivce(ObjectiveRepository objectiveRepository, UserService userService, EntityManager entityManager) {
+        return new ObjectiveServiceImpl(objectiveRepository,userService,entityManager);
     }
 
 }
