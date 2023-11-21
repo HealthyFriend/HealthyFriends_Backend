d78e89992fd9c3b89ff2dac7a00a957ad67ad364
DDeo99 <yoondukwoo@gmail.com>
2023-11-21 19:39:24 +0900

    validation 의존성 추가

--- src/main/java/server/healthyFriends/web/controller/LoginController.java
+++ src/main/java/server/healthyFriends/web/controller/LoginController.java
@@ -1,13 +1,18 @@
 package server.healthyFriends.web.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import server.healthyFriends.config.AppConfig;
+import server.healthyFriends.config.SecurityConfig;
 import server.healthyFriends.domain.User;
+import server.healthyFriends.domain.dto.JoinRequest;
 import server.healthyFriends.domain.dto.LoginRequest;
 import server.healthyFriends.domain.dto.ResponseDTO;
+import server.healthyFriends.repository.UserRepository;
 import server.healthyFriends.sercurity.jwt.JwtTokenUtil;
 import server.healthyFriends.service.UserService;
 import server.healthyFriends.service.UserServiceImpl;
@@ -15,10 +20,38 @@ import server.healthyFriends.web.response.ResponseUtil;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/jwt-login")
+@RequestMapping("/users")
 public class LoginController {
 
+    private final UserRepository userRepository;
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/join")
+    public ResponseDTO<String> join(@RequestBody JoinRequest joinRequest) {
+        try {
+            //loginId 중복 체크
+            if (userService.checkLoginIdDuplicate(joinRequest.getLoginId())) {
+                return ResponseUtil.conflict("이미 존재하는 아이디입니다.", null);
+            }
+
+            if(userService.checkNicknameDuplicate(joinRequest.getNickname())) {
+                return ResponseUtil.conflict("이미 존재하는 닉네임입니다.",null);
+            }
+
+            if (!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())) {
+                return ResponseUtil.badRequest("비밀번호와 비밀번호 확인이 일치하지 않습니다.",null);
+            }
+
+            userService.join(joinRequest);
+            return ResponseUtil.success("회원가입에 성공하셨습니다.",null);
+
+        } catch(Exception e) {
+            throw e;
+        }
+    }
+
+
 
     @PostMapping("/login")
     public ResponseDTO<String> login(@RequestBody LoginRequest loginRequest) {
@@ -26,6 +59,7 @@ public class LoginController {
         User user = userService.login(loginRequest);
 
         try {
+
             // 로그인 아이디나 비밀번호가 틀린 경우 global error return
             if (user == null) {
                 return ResponseUtil.unauthorized("로그인 아이디 또는 비밀번호가 틀렸습니다.", null);
@@ -43,5 +77,10 @@ public class LoginController {
         } catch(Exception e) {
             throw e;
         }
+    }
+
+    @GetMapping("/admin")
+    public ResponseDTO<String> adminPage() {
+        return ResponseUtil.success("관리자 페이지 접근",null);
     }
 }
