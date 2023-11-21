768412697a9c83d604bf76a404556cb4d11cb0de
DDeo99 <yoondukwoo@gmail.com>
2023-11-21 19:40:47 +0900

    회원가입 DTO

---
+++ src/main/java/server/healthyFriends/domain/dto/JoinRequest.java
@@ -0,0 +1,53 @@
+package server.healthyFriends.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import server.healthyFriends.domain.User;
+import server.healthyFriends.domain.enums.Gender;
+import server.healthyFriends.domain.enums.Role;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+public class JoinRequest {
+
+    @NotBlank(message="아이디를 입력하세요.")
+    private String loginId;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    private String password;
+    private String passwordCheck;
+
+    @NotBlank(message = "이름을 입력하세요." )
+    private String name;
+
+    @NotBlank(message = "닉네임을 입력하세요." )
+    private String nickname;
+
+    @NotNull(message = "키를 입력하세요." )
+    private BigDecimal height;
+
+    @NotNull(message = "나이를 입력하세요." )
+    private Integer age;
+
+    @NotNull(message = "성별을 입력하세요." )
+    private Gender gender;
+
+    // 비밀번호 암호화하여 저장
+    public User toEntity(String encodedPassword) {
+        return User.builder()
+                .loginId(this.loginId)
+                .password(encodedPassword)
+                .nickname(this.nickname)
+                .name(this.name)
+                .age(this.age)
+                .height(this.height)
+                .gender(this.gender)
+                .role(Role.USER)
+                .build();
+    }
+}
+
