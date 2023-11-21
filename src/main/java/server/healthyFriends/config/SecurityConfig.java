186c1940bf1a3f356186d7c14acffb9a87d88645
DDeo99 <yoondukwoo@gmail.com>
2023-11-21 15:58:02 +0900

    인증 필요 엔드포인트 수정

--- src/main/java/server/healthyFriends/config/SecurityConfig.java
+++ src/main/java/server/healthyFriends/config/SecurityConfig.java
@@ -23,16 +23,35 @@ public class SecurityConfig {
     private final UserService userService;
     private static String secretKey = "my-secret-key-20220121";
 
+    // securityFilterChain 이름의 SecurityFilterChain 타입의 빈 반환
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        /**
+         * HTTP 기본 인증 비활성화
+         * CSRF 보호 비활성화
+         * 세션 관리 설정
+         * JwtTokenFilter 추가
+         * 권한 규칙 구성 추가
+         * 특정 엔드포인트에 대한 인증 필요한 경우 -> 해당 엔드포인트에 대해 인증 필요함 설정
+         * ADMIN 권한을 가진 사용자에게 특정 URL에 액세스할 권한 필요한 경우 -> 허용
+         * 권한 규칙 구성 종료
+         */
         return httpSecurity
+                //HTTP 기본 인증 비활성화
                 .httpBasic().disable()
+                //CSRF(Cross-Site Request Forgery) 보호 비활성화
                 .csrf().disable()
+                //세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //보안 구성 결합
                 .and()
+                //JwtTokenFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                //권한 규칙 구성 시작
                 .authorizeRequests()
-                .antMatchers("/jwt-login/info").authenticated()
+                ///jwt-login/info 엔드포인트에 대한 인증 필요
+                //.antMatchers("/").authenticated()
+                //ADMIN 권한 가진 사용자에게 /jwt-login/admin/ 하우의 모든 URL에 액세스할 권한 필요
                 .antMatchers("/jwt-login/admin/**").hasAuthority(Role.ADMIN.name())
                 .and().build();
     }
