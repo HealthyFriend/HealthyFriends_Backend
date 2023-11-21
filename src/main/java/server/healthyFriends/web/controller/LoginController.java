66b9ed877d698dfb960a8dc22c3f57191d5ef464
DDeo99 <yoondukwoo@gmail.com>
2023-11-21 17:23:32 +0900

    로그인 컨트롤러 수정

--- src/main/java/server/healthyFriends/web/controller/LoginController.java
+++ src/main/java/server/healthyFriends/web/controller/LoginController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import server.healthyFriends.config.AppConfig;
 import server.healthyFriends.domain.User;
 import server.healthyFriends.domain.dto.LoginRequest;
+import server.healthyFriends.domain.dto.ResponseDTO;
 import server.healthyFriends.sercurity.jwt.JwtTokenUtil;
 import server.healthyFriends.service.UserService;
 import server.healthyFriends.service.UserServiceImpl;
+import server.healthyFriends.web.response.ResponseUtil;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,22 +21,27 @@ public class LoginController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseDTO<String> login(@RequestBody LoginRequest loginRequest) {
 
         User user = userService.login(loginRequest);
 
-        // 로그인 아이디나 비밀번호가 틀린 경우 global error return
-        if(user == null) {
-            return"로그인 아이디 또는 비밀번호가 틀렸습니다.";
+        try {
+            // 로그인 아이디나 비밀번호가 틀린 경우 global error return
+            if (user == null) {
+                return ResponseUtil.unauthorized("로그인 아이디 또는 비밀번호가 틀렸습니다.", null);
+            }
+
+            // 로그인 성공 => Jwt Token 발급
+
+            String secretKey = "my-secret-key-20220121";
+            long expireTimeMs = 1000 * 60 * 60;     // Token 유효 시간 = 60분
+
+            String jwtToken = JwtTokenUtil.createToken(user.getLoginId(), secretKey, expireTimeMs);
+
+            return ResponseUtil.success("로그인 성공", jwtToken);
+
+        } catch(Exception e) {
+            throw e;
         }
-
-        // 로그인 성공 => Jwt Token 발급
-
-        String secretKey = "my-secret-key-20220121";
-        long expireTimeMs = 1000 * 60 * 60;     // Token 유효 시간 = 60분
-
-        String jwtToken = JwtTokenUtil.createToken(user.getLoginId(), secretKey, expireTimeMs);
-
-        return jwtToken;
     }
 }
