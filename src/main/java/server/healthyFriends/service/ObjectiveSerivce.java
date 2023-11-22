ee400195bef85e840c0516f39ca583f463212df3
DDeo99 <yoondukwoo@gmail.com>
2023-11-23 00:34:20 +0900

    목표 조회, 삭제 추가

--- src/main/java/server/healthyFriends/service/ObjectiveSerivce.java
+++ src/main/java/server/healthyFriends/service/ObjectiveSerivce.java
@@ -5,6 +5,7 @@ import server.healthyFriends.domain.dto.ObjectiveRequest;
 
 public interface ObjectiveSerivce {
     public Objective createObjective(Long userId, ObjectiveRequest objectiveRequest);
-
+    public Objective readObjective(Long objectiveId);
     public Objective updateObjective(Long objectiveId, ObjectiveRequest objectiveRequest);
+    public void deleteObjective(Long objectiveId);
 }
--- src/main/java/server/healthyFriends/service/ObjectiveServiceImpl.java
+++ src/main/java/server/healthyFriends/service/ObjectiveServiceImpl.java
@@ -8,6 +8,7 @@ import server.healthyFriends.domain.dto.ObjectiveRequest;
 import server.healthyFriends.repository.ObjectiveRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 @Service
@@ -38,11 +39,20 @@ public class ObjectiveServiceImpl implements ObjectiveSerivce{
         return savedObjective;
     }
 
+    // 목표 조회
+    public Objective readObjective(Long objectiveId) {
+
+        Objective existingObjective = objectiveRepository.findById(objectiveId)
+                .orElseThrow(()->new EntityNotFoundException("해당하는 목표를 찾을 수 없습니다."));
+
+        return existingObjective;
+    }
+
     // 목표 수정
     public Objective updateObjective(Long objectiveId, ObjectiveRequest objectiveRequest) {
 
         Objective existingObjective = objectiveRepository.findById(objectiveId)
-                .orElseThrow(() -> new RuntimeException("해당하는 목표를 찾을 수 없습니다."));
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 목표를 찾을 수 없습니다."));
 
         existingObjective.setStart_day(objectiveRequest.getStart_day());
         existingObjective.setEnd_day(objectiveRequest.getEnd_day());
@@ -55,4 +65,14 @@ public class ObjectiveServiceImpl implements ObjectiveSerivce{
         return updatedObjective;
 
     }
+
+    // 목표 삭제
+    public void deleteObjective(Long objectiveId) {
+
+        Objective existingObjective = objectiveRepository.findById(objectiveId)
+                .orElseThrow(()->new EntityNotFoundException("해당하는 목표를 찾을 수 없습니다."));
+
+        objectiveRepository.delete(existingObjective);
+    }
+
 }
