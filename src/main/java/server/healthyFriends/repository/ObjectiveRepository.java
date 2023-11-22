9503c7b1775c99e276691caeb194cdb247cd4cc3
DDeo99 <yoondukwoo@gmail.com>
2023-11-23 02:56:45 +0900

    Optional<List<Objective>> findByUserId(Long userId) 목표 리스트 조회

--- src/main/java/server/healthyFriends/repository/ObjectiveRepository.java
+++ src/main/java/server/healthyFriends/repository/ObjectiveRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import server.healthyFriends.domain.entity.Objective;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ObjectiveRepository extends JpaRepository<Objective, Long> {
 
     Optional<Objective> findById(Long objectiveId);
+
+    Optional<List<Objective>> findByUserId(Long userId);
 }
