40885945348c443c2398173e226a426eeba506fd
DDeo99 <yoondukwoo@gmail.com>
2023-11-24 02:01:12 +0900

    친구 요청 response DTO

---
+++ src/main/java/server/healthyFriends/domain/dto/FriendResponse.java
@@ -0,0 +1,18 @@
+package server.healthyFriends.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FriendResponse {
+
+    private Long id;
+
+    private Long requester_id;
+
+    private Long recipient_id;
+
+}
