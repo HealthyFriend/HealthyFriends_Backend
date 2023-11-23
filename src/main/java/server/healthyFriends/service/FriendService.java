1b57d4c44b51dcdf57993d4b5f397fa378ccddab
DDeo99 <yoondukwoo@gmail.com>
2023-11-24 00:19:06 +0900

    친구 신청 수락 기능

--- src/main/java/server/healthyFriends/service/FriendService.java
+++ src/main/java/server/healthyFriends/service/FriendService.java
@@ -4,4 +4,7 @@ import server.healthyFriends.domain.dto.FriendRequest;
 
 public interface FriendService {
     public FriendRequest requestFriend(String friend_loginId, Long userId);
+
+    public void acceptFriend(Long friendMappingId, Long request_userId, Long recipient_userId);
+
 }
--- src/main/java/server/healthyFriends/service/FriendServiceImpl.java
+++ src/main/java/server/healthyFriends/service/FriendServiceImpl.java
@@ -36,13 +36,37 @@ public class FriendServiceImpl implements FriendService{
         friendMapping.setStatus(false); // 초기 상태는 false (미수락 상태)
         friendMapping = friendRepository.save(friendMapping);
 
-        //Request
+        //친구 요청 성공 시 반환 DTO
         FriendRequest friendRequest = new FriendRequest();
         friendRequest.setRequester_id(requestUser.getId());
         friendRequest.setRecipient_id(recipientUser.getId());
+        friendRequest.setId(friendMapping.getId());
 
         return friendRequest;
     }
 
+    // 친구 수락
+    public void acceptFriend(Long friendMappingId, Long request_userId, Long recipient_userId) {
 
+        User requestUser = userRepository.findById(request_userId)
+                .orElseThrow(()->new EntityNotFoundException("해당하는 유저가 없습니다."));
+
+        User recipientUser = userRepository.findById(recipient_userId)
+                .orElseThrow(()->new EntityNotFoundException("해당하는 유저가 없습니다."));
+
+
+        FriendMapping friendMapping = friendRepository.findById(friendMappingId)
+                .orElseThrow(()->new EntityNotFoundException("해당하는 엔티티가 없습니다."));
+
+        // 친구 상태 true로 변환
+        friendMapping.setStatus(true);
+        friendRepository.save(friendMapping);
+
+        // 역순으로 테이블에 하나 더 저장
+        FriendMapping friendMappingReverse = new FriendMapping();
+        friendMappingReverse.setUser(recipientUser);
+        friendMappingReverse.setFriendId(requestUser.getId());
+        friendMappingReverse.setStatus(true);
+        friendRepository.save(friendMappingReverse);
+    }
 }
