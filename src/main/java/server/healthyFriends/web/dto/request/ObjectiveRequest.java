eb6b896d1d496db80ee57538757a027b76f2b0d8
DDeo99 <yoondukwoo@gmail.com>
2023-12-04 04:52:13 +0900

    request, response 한 클래스 내에 static 메서드 배치

--- src/main/java/server/healthyFriends/web/dto/request/ObjectiveRequest.java
+++ src/main/java/server/healthyFriends/web/dto/request/ObjectiveRequest.java
@@ -1,4 +1,4 @@
-package server.healthyFriends.web.dto.request.objective;
+package server.healthyFriends.web.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/server/healthyFriends/web/dto/response/ObjectiveResponse.java
@@ -0,0 +1,46 @@
+package server.healthyFriends.web.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ObjectiveResponse {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SingleObjectiveResponse {
+        private LocalDate start_Day;
+        private LocalDate end_Day;
+        private String head;
+        private String body;
+        private Boolean status;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ListObjectiveResponse {
+        List<ObjectiveResponse.SingleObjectiveResponse> objectiveList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateObjectiveResultDTO {
+        Long objectiveId;
+        LocalDateTime createdAt;
+    }
+
+
+}
