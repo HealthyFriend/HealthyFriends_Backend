1e55b2f7eb1bc872484d730f5b499423f47344bc
DDeo99 <yoondukwoo@gmail.com>
2023-12-03 22:03:34 +0900

    사용자 정보 가져오는 UserDetailService 구현체

---
+++ src/main/java/server/healthyFriends/sercurity/jwt/CustomUserDetailsImpl.java
@@ -0,0 +1,27 @@
+package server.healthyFriends.sercurity.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.healthyFriends.domain.entity.User;
+import server.healthyFriends.repository.UserRepository;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CustomUserDetailsImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저가 없습니다."));
+
+        return new CustomUserDetails(user) {
+        };
+    }
+
+}
