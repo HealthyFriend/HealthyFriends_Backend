936670a28616e1b67ad152dea92876511149e3f9
DDeo99 <yoondukwoo@gmail.com>
2023-12-04 05:58:36 +0900

    WebConfig

---
+++ src/main/java/server/healthyFriends/config/WebConfig.java
@@ -0,0 +1,17 @@
+package server.healthyFriends.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/objectives/**")
+                .allowedOrigins("http://localhost:8080")  // 허용할 오리진 추가
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
