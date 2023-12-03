0a24ae838c4bfa19ed06d650b6ad1e8d76191896
DDeo99 <yoondukwoo@gmail.com>
2023-12-03 22:14:44 +0900

    EntityDtoMapperImpl

---
+++ src/main/generated/server/healthyFriends/web/response/EntityDtoMapperImpl.java
@@ -0,0 +1,30 @@
+package server.healthyFriends.web.response;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import server.healthyFriends.domain.entity.Objective;
+import server.healthyFriends.web.dto.ObjectiveResponse;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-12-03T19:02:26+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.9 (Oracle Corporation)"
+)
+@Component
+public class EntityDtoMapperImpl implements EntityDtoMapper {
+
+    @Override
+    public ObjectiveResponse objectivetoDto(Objective objective) {
+        if ( objective == null ) {
+            return null;
+        }
+
+        ObjectiveResponse objectiveResponse = new ObjectiveResponse();
+
+        objectiveResponse.setHead( objective.getHead() );
+        objectiveResponse.setBody( objective.getBody() );
+        objectiveResponse.setStatus( objective.getStatus() );
+
+        return objectiveResponse;
+    }
+}
