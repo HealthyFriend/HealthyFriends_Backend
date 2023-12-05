fcb8bfe0b883aaad85c4221dfe5587ffae04d0d9
DDeo99 <yoondukwoo@gmail.com>
2023-12-06 01:36:55 +0900

    JWT 검증 테스트

--- src/main/java/server/healthyFriends/JwtDecoderTest.java
+++ src/main/java/server/healthyFriends/JwtDecoderTest.java
@@ -1,32 +1,39 @@
 package server.healthyFriends;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class JwtDecoderTest {
     public static void main(String[] args) {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcwMTYxMTM4OSwiZXhwIjoxNzA1MjExMzg5fQ.TJVETyyrhk2olqCVxlsW1PyBP34OAq3h1L-P2lRHWAs";
-        // 토큰을 디코딩하여 헤더, 페이로드, 시그니처를 출력
-        String[] parts = token.split("\\.");
-        String header = new String(Base64.getUrlDecoder().decode(parts[0]), StandardCharsets.UTF_8);
-        String payload = new String(Base64.getUrlDecoder().decode(parts[1]), StandardCharsets.UTF_8);
-        String signature = parts[2];
+        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcwMTc5MzE1MiwiZXhwIjoxNzA1MzkzMTUyfQ.88BknIrDqEYbFUWaVZAW3GsfJrnmfsVlQYIVNv7vWtk";
 
-        // 결과 출력
-        System.out.println("Header: " + header);
-        System.out.println("Payload: " + payload);
-        System.out.println("Signature: " + signature);
+      String sk = "MySecretKeyIs20220121PluSafasgdsgasgzsczgsgasgfsbzcbzbvzbnznd";
+      SecretKey secretKey = Keys.hmacShaKeyFor(sk.getBytes(StandardCharsets.UTF_8));
 
-        // 시크릿 키를 사용하여 토큰을 디코딩
-        Claims claims = Jwts.parser().setSigningKey("MySecretKeyIs20220121PluSafasgdsgasgzsczgsgasgfsbzcbzbvzbnznd").parseClaimsJws(token).getBody();
+        // JWT 토큰 검증
+        try {
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(secretKey) // 토큰을 서명한 키 (Secret Key)
+                    .parseClaimsJws(removeBearerPrefix(jwtToken)); // "Bearer " 제거
 
-        // 토큰의 클레임에서 필요한 정보 추출
-        Long userId = claims.get("userId", Long.class);
+            // 토큰이 정상적으로 검증되었을 때의 로직
+            Claims body = claimsJws.getBody();
+            System.out.println("User ID: " + body.get("userId"));
+        } catch (Exception e) {
+            // 토큰 검증 실패 시의 예외 처리
+            e.printStackTrace();
+        }
+    }
 
-        // 결과 출력
-        System.out.println("Decoded ID: " + userId);
+    private static String removeBearerPrefix(String token) {
+        // "Bearer " 부분을 제거하여 실제 토큰 값만 반환
+        return token.replace("Bearer ", "");
     }
 }
