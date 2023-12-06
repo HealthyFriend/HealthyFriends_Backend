e0611b30445e9d7df7464fe3d669c9ebfd12acf1
DDeo99 <yoondukwoo@gmail.com>
2023-12-07 01:50:56 +0900

    Swagger @Schema 애노테이션 추가

--- src/main/java/server/healthyFriends/apiPayload/ResponseDTO.java
+++ src/main/java/server/healthyFriends/apiPayload/ResponseDTO.java
@@ -1,5 +1,6 @@
 package server.healthyFriends.apiPayload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import server.healthyFriends.domain.enums.ResponseStatus;
@@ -9,9 +10,13 @@ import server.healthyFriends.domain.enums.ResponseStatus;
 @AllArgsConstructor
 public class ResponseDTO<T> {
 
+    @Schema(description = "상태 코드", nullable = false)
     private final int code;
+    @Schema(description = "HttpStatus", nullable = false)
     private final ResponseStatus status;
+    @Schema(description = "상태 메시지", nullable = true)
     private final String message;
+    @Schema(description = "데이터", nullable = true)
     private final T data;
 
 }
