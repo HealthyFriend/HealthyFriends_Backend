7216a1fe7e41e3e471f9fefc372674cff0932eea
DDeo99 <yoondukwoo@gmail.com>
2023-12-06 20:19:54 +0900

    사용하지 않는 설정 제거

--- src/main/java/server/healthyFriends/config/WebConfig.java
+++ src/main/java/server/healthyFriends/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/objectives/**")
+        registry.addMapping("")
                 .allowedOrigins("http://localhost:8080")  // 허용할 오리진 추가
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
