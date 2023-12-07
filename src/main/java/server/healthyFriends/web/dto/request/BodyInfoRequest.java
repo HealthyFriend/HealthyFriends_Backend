b3aca27be3b7aaa39df4fe00fe38bfb5c9b467ca
DDeo99 <yoondukwoo@gmail.com>
2023-12-07 21:35:40 +0900

    체성분 기록 수정 Request DTO

--- src/main/java/server/healthyFriends/web/dto/request/BodyInfoRequest.java
+++ src/main/java/server/healthyFriends/web/dto/request/BodyInfoRequest.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class BodyInfoRequest {
 
@@ -29,5 +30,26 @@ public class BodyInfoRequest {
         @Schema(description = "체지방", nullable = true, example = "20.0")
         private BigDecimal body_fat_mass;
 
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class UpdateBodyInfoRequest {
+
+        @DecimalMin(value="30.0") @DecimalMax(value="150.0")
+        @Schema(description = "몸무게", nullable = true, example = "70.0")
+        private BigDecimal weight;
+
+        @DecimalMin(value="5.0") @DecimalMax(value="90.0")
+        @Schema(description = "골격근량", nullable = true, example = "35.0")
+        private BigDecimal skeletal_muscle_mass;
+
+        @DecimalMin(value="5.0") @DecimalMax(value="90.0")
+        @Schema(description = "체지방", nullable = true, example = "25.0")
+        private BigDecimal body_fat_mass;
+
+        @Schema(description = "수정할 날짜", nullable = false, example = "2023-12-07")
+        private LocalDate edit_day;
     }
 }
